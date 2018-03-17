79050c494a4dbb5e305207b574d0f2bd439339d0
Stanisław Puławski <sosnus15@gmail.com>
2018-03-17 16:06:54 +0100

    unzip rar

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=tulbotcore-Web-Deploy.pubxml -p:Password=68BTwaXlFAedFheXW3cdpsfjFyp8BZi4KHZknBBehbturiqEkKQnouZf5XQq
+
