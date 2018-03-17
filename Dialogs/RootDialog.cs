a6548adfa2bd40fc1481fdb7ac744da1f7e81dc7
badfilling <truemob2@gmail.com>
2018-03-17 21:05:01 +0100

    wykladowca command
    added command

--- Dialogs/RootDialog.cs
+++ Dialogs/RootDialog.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Net.Http;
-
+using SimpleEchoBot.Models;
 
 namespace SimpleEchoBot.Dialogs
 {
@@ -42,9 +42,24 @@ namespace SimpleEchoBot.Dialogs
                 }
                 if (message.Text.ToLower().Contains("wykladowca") || message.Text.ToLower().Contains("wykładowca"))
                 {
+                    
                     //11 to skip wykladowca + space
                     content = message.Text.Substring(11);
                     //TODO go to db and find record then send it to user
+                    using (var dbcontext = new tulbotdevEntities1())
+                    {
+                        // Query for all blogs with names starting with B 
+                        var teacher = dbcontext.peoples
+                            .Where(b => b.Surname == content)
+                            .FirstOrDefault();
+                        if (teacher != null)
+                        {
+                            var depart = dbcontext.department.Find(teacher.Department);
+                            await context.PostAsync($"{teacher.Title} {teacher.Name} {teacher.Surname}," +
+                                $"  jednostka:{depart.Short} , konsultacje: {teacher.Consultation}, telefon: {teacher.Tel}");
+                        }
+                        else await context.PostAsync("Nie znalazłem nikogo z takim nazwiskiem");
+                    }
                 }
                 else if (message.Text.ToLower().Contains("jednostka"))
                 {
